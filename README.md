92a4b22c577714c692485fb4f485e328d858fc32
Tyrone Bowman <88301291+TytheBowman@users.noreply.github.com>
2023-04-25 19:20:30 -0400

    mvp hopefully

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Tys-social-newtork-api
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)     
+This is the backend of a social media application that can perform various user-related operations, including displaying friendship connections. It enables users to create and modify posts, as well as react to other users' posts.
+
+
+Table of Contents
+=================
+* [Installation Instructions](#installation-instructions)
+* [Usage Instructions](#usage)
+
+
+## Installation Instructions
+1.  Clone the reop and npm i .
+
+## Usage
+The app should be started at a terminal prompt by typing "node index.js"
+
+
+
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,6 @@
 // Importing the required functions from the userController file
+const userController = require('../../controllers/userController');
+
 const {
     getUsers,
     getSingleUser,
@@ -7,19 +9,22 @@ const {
     deleteUser,
     addFriend,
     deleteFriend
-    } = require('../../controllers/userController');
-    
-    // Creating a new router instance
-    const userRouter = require('express').Router();
-    
-    // Defining the API routes and their respective HTTP methods
-    userRouter.get('/users', getUsers); // Route to get all users
-    userRouter.post('/users', createUser); // Route to create a new user
-    userRouter.get('/users/:userId', getSingleUser); // Route to get a single user by ID
-    userRouter.put('/users/:userId', updateUser); // Route to update a single user by ID
-    userRouter.delete('/users/:userId', deleteUser); // Route to delete a single user by ID
-    userRouter.post('/users/:userId/friends/:friendId', addFriend); // Route to add a friend to a user
-    userRouter.delete('/users/:userId/friends/:friendId', deleteFriend); // Route to delete a friend from a user
-    
-    // Exporting the router instance
-    module.exports = userRouter;
+} = userController;
+
+
+// Creating a new router instance
+const userRouter = require('express').Router();
+
+// Defining the API routes and their respective HTTP methods
+userRouter.get('/users', getUsers); // Route to get all users
+userRouter.post('/users', createUser); // Route to create a new user
+userRouter.get('/users/:userId', getSingleUser); // Route to get a single user by ID
+userRouter.put('/users/:userId', updateUser); // Route to update a single user by ID
+userRouter.delete('/users/:userId', deleteUser); // Route to delete a single user by ID
+userRouter.post('/users/:userId/friends/:friendId', addFriend); // Route to add a friend to a user
+userRouter.delete('/users/:userId/friends/:friendId', deleteFriend); // Route to delete a friend from a user
+
+// Exporting the router instance
+module.exports = userRouter;
+
+  
